4df0f3211fd53d1bd8c3af888cf0e5dbb17b1e56
German Montiel <GMontiel@originsoftware.com.ar>
2020-05-12 17:07:40 -0300

    render con jsx

--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// const element = document.createElement('h1')
+// element.innerText= 'Hola React'
+// no cree el elemento div xq ya lo tengo en public/index.html 
+// const container = document.getElementById('root')
+// agregamos a ese div el elemento "h1"
+// container.appendChild(element)
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+const user = {
+  firstName: 'German',
+  lastName: 'Montiel',
+  avatar: 'https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/batman_hero_avatar_comics-256.png'
+}
+
+function getName(user) {
+  return `${user.firstName} ${user.lastName}`
+}
+
+function getValidateUs(user) {
+  if (user) {
+    return <h1>Hello {getName(user)}</h1>
+  }
+  return <h1> Hello Stranger </h1>
+}
+
+
+// const name = 'German'
+
+const element = (
+  <div >
+    <h1> {getValidateUs(user)} </h1>
+    <img src={user.avatar} />
+  </div >
+)
+const container = document.getElementById('root')
+
+// ReactDOM.render(__QUE_ELEMENTO, __DONDE__)
+
+ReactDOM.render(element, container) 
